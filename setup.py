4d3308c6c00a66eb3191d210a42dbb83a4e82b0a
Jonathan Reed <jdreed@mit.edu>
2013-11-04 13:38:12 -0500

    Move script name logic to setup.py
    Rather than renaming the script in the rules file, move the logic
    to setup.py itself, making use of custom distutils commands to
    do the work for us.

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 from distutils.core import setup
-import sys
+from distutils.command.build_scripts import build_scripts
+from distutils.command.clean import clean
+import sys, os, shutil, filecmp
 
 extra_options = {}
 
-# The script is named this on all platforms except OS X
-script_name='pyhesiodfs'
 if sys.platform == 'darwin':
     extra_options['data_files'] = [('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))]
-    script_name='pyHesiodFS.py
+    copy_file = None
+    script_name = 'pyHesiodFS.py'
+else:
+    script_name='pyhesiodfs'
+    copy_file = ('pyHesiodFS.py', 'pyhesiodfs')
+
+class BuildScriptsCommand(build_scripts):
+    def finalize_options(self):
+        build_scripts.finalize_options(self)
+        if copy_file is not None:
+            if os.path.isfile(copy_file[1]) and not filecmp.cmp(*copy_file, shallow=False):
+                raise Exception("Will not overwrite existing '%s' with '%s'" % tuple(reversed(copy_file)))
+            shutil.copyfile(*copy_file)
+
+class CleanCommand(clean):
+    def finalize_options(self):
+        clean.finalize_options(self)
+        if copy_file is not None and os.path.isfile(copy_file[1]):
+            if not filecmp.cmp(*copy_file, shallow=False):
+                raise Exception("Will not remove '%s', it's not the same as '%s'" % tuple(reversed(copy_file)))
+            os.unlink(copy_file[1])
 
 setup(name='pyHesiodFS',
       version='1.0.1',
@@ -17,4 +37,8 @@ setup(name='pyHesiodFS',
       maintainer_email='debathena@mit.edu',
       scripts=[script_name],
       requires=['PyHesiod (>=0.2.0)'],
+      cmdclass={
+        'clean': CleanCommand,
+        'build_scripts': BuildScriptsCommand,
+        },
       **extra_options)
