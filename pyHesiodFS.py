63feb4ee62d39dd46d364ef6e259ed195d33d448
Jonathan Reed <jdreed@mit.edu>
2013-11-14 14:26:34 -0500

    New upstream release with incorporated patches
    As we are now becoming upstream for PyHesiodFS, which involves:
     - apply all our patches to pyHesiodFS.py
     - delete the patch files
     - punt the dependencies on quilt and patchutils in debian/control
     - punt the patchsys-quilt rule in debian/rules
     - instead of patching setup.py to change the script name, add some
       Python code to retain the old script name on OS X only
     - Add a debian/source/format file, since we didn't have one (force this
       to be source format 1.0 until we deal)

--- pyHesiodFS.py
+++ pyHesiodFS.py
@@ -72,13 +72,6 @@ new_fuse = hasattr(fuse, '__version__')
 
 fuse.fuse_python_api = (0, 2)
 
-hello_path = '/README.txt'
-hello_str = """This is the pyhesiodfs FUSE autmounter. To access a Hesiod filsys, just access
-%(mountpoint)s/name.
-
-If you're using the Finder, try pressing Cmd+Shift+G and then entering
-%(mountpoint)s/name"""
-
 if not hasattr(fuse, 'Stat'):
     fuse.Stat = object
 
@@ -138,10 +131,6 @@ class PyHesiodFS(Fuse):
             st.st_mode = stat.S_IFDIR | 0755
             st.st_gid = self._gid()
             st.st_nlink = 2
-        elif path == hello_path:
-            st.st_mode = stat.S_IFREG | 0444
-            st.st_nlink = 1
-            st.st_size = len(hello_str)
         elif '/' not in path[1:]:
             if path[1:] not in self.negcache[self._uid()] and self.findLocker(path[1:]):
                 st.st_mode = stat.S_IFLNK | 0777
@@ -176,19 +165,22 @@ class PyHesiodFS(Fuse):
             if len(filsys.filsys) >= 1:
                 pointers = filsys.filsys
                 pointer = pointers[0]
-                if pointer['type'] != 'AFS' and pointer['type'] != 'LOC':
-                    syslog(LOG_NOTICE, "Unknown locker type "+pointer['type']+" for locker "+name+" ("+repr(pointer)+" )")
-                    return None
-                else:
+                if pointer['type'] == 'AFS' or pointer['type'] == 'LOC':
                     self.mounts[self._uid()][name] = pointer['location']
                     syslog(LOG_INFO, "Mounting "+name+" on "+pointer['location'])
                     return pointer['location']
+                elif pointer['type'] == 'ERR':
+                    syslog(LOG_NOTICE, "ERR for locker %s: %s" % (name, pointer['message'], ))
+                    return None
+                else:
+                    syslog(LOG_NOTICE, "Unknown locker type "+pointer['type']+" for locker "+name+" ("+repr(pointer)+" )")
+                    return None
             else:
                 syslog(LOG_WARNING, "Couldn't find filsys for "+name)
                 return None
 
     def getdir(self, path):
-        return [(i, 0) for i in (['.', '..', hello_path[1:]] + self.getCachedLockers())]
+        return [(i, 0) for i in (['.', '..'] + self.getCachedLockers())]
 
     def readdir(self, path, offset):
         for (r, zero) in self.getdir(path):
@@ -197,27 +189,8 @@ class PyHesiodFS(Fuse):
     def readlink(self, path):
         return self.findLocker(path[1:])
 
-    def open(self, path, flags):
-        if path != hello_path:
-            return -errno.ENOENT
-        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-        if (flags & accmode) != os.O_RDONLY:
-            return -errno.EACCES
-
-    def read(self, path, size, offset):
-        if path != hello_path:
-            return -errno.ENOENT
-        slen = len(hello_str)
-        if offset < slen:
-            if offset + size > slen:
-                size = slen - offset
-            buf = hello_str[offset:offset+size]
-        else:
-            buf = ''
-        return buf
-    
     def symlink(self, src, path):
-        if path == '/' or path == hello_path:
+        if path == '/':
             return -errno.EPERM
         elif '/' not in path[1:]:
             self.mounts[self._uid()][path[1:]] = src
@@ -226,7 +199,7 @@ class PyHesiodFS(Fuse):
             return -errno.EPERM
     
     def unlink(self, path):
-        if path == '/' or path == hello_path:
+        if path == '/':
             return -errno.EPERM
         elif '/' not in path[1:]:
             del self.mounts[self._uid()][path[1:]]
@@ -235,7 +208,6 @@ class PyHesiodFS(Fuse):
             return -errno.EPERM
 
 def main():
-    global hello_str
     try:
         usage = Fuse.fusage
         server = PyHesiodFS(version="%prog " + fuse.__version__,
@@ -251,8 +223,12 @@ pyHesiodFS [mountpath] [options]
             sys.argv.pop(1)
         server = PyHesiodFS()
 
-    hello_str = hello_str % {'mountpoint': server.parse(errex=1).mountpoint}
-    server.main()
+    try:
+        server.main()
+    except fuse.FuseError as fe:
+        print >>sys.stderr, "An error occurred while starting PyHesiodFS:"
+        print >>sys.stderr, fe
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ import sys
 
 extra_options = {}
 
+# The script is named this on all platforms except OS X
+script_name='pyhesiodfs'
 if sys.platform == 'darwin':
     extra_options['data_files'] = [('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))]
+    script_name='pyHesiodFS.py
 
 setup(name='pyHesiodFS',
-      version='1.0',
+      version='1.0.1',
       author='Quentin Smith',
       author_email='pyhesiodfs@mit.edu',
-      scripts=['pyHesiodFS.py'],
+      maintainer='Debathena Project',
+      maintainer_email='debathena@mit.edu',
+      scripts=[script_name],
       requires=['PyHesiod (>=0.2.0)'],
       **extra_options)
